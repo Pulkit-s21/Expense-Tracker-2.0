eca21570d5a2d9ed7cafdfcd88a32e429d46f1b3
Pulkit-s21 <pulkitsaxena795@gmail.com>
2025-04-27 17:36:40 +0530

    Added protected route. Task list updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { Home } from "./pages/Home.jsx"
 import Signup from "./pages/Signup.jsx"
 import Login from "./pages/Login.jsx"
+import ProtectedRoutes from "./components/ProtectedRoutes.jsx"
 
 function App() {
   return (
     <Router>
       {/* <Navbar /> */}
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<ProtectedRoutes element={<Home />} />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
       </Routes>
@@ -22,3 +23,5 @@ export default App
 
 // : Get site up and running : DONE
 // TODO: Find a way to show good errors from backend
+// : Make protected routes : DONE
+// TODO: Start designing Homepage
