6bad955df441e687d9f063aeb9cb94ff5d99cc21
Pulkit-s21 <pulkitsaxena795@gmail.com>
2025-04-28 16:39:54 +0530

    Income, transaction API created

---
+++ backend/src/routes/incomeRoutes.js
@@ -0,0 +1,112 @@
+import { Router } from "express"
+import { incomeValidation } from "../validator/validations.js"
+import { validationError } from "../validator/error.js"
+import prisma from "../prismaClient.js"
+
+const router = Router()
+
+// create
+router.post("/create", async (req, res) => {
+  try {
+    const { error, value } = incomeValidation.validate(req.body)
+
+    if (error) {
+      return validationError(res, error)
+    }
+
+    const { title, userId, date, amount } = value
+
+    if (req.userId !== userId) {
+      return res.status(400).json({ message: "Forbidden. Access Denied !!" })
+    }
+
+    const income = await prisma.income.create({
+      data: {
+        title,
+        userId,
+        date,
+        amount,
+      },
+    })
+
+    if (!income || income.id) {
+      return res.status(400).json({ message: "Failed to add income !!" })
+    }
+
+    res.json({ message: "Income added !!" })
+  } catch (err) {
+    console.error(err.message)
+    return res.status(503).json({ message: err.message })
+  }
+})
+
+// read
+router.get("/read/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params
+
+    if (req.userId !== parseInt(userId)) {
+      return res.status(400).json({ message: "Forbidden. Access Denied !!" })
+    }
+
+    const allIncomes = await prisma.income.findMany({
+      where: { userId: parseInt(userId) },
+    })
+
+    if (!allIncomes)
+      return res.status(404).json({ message: "No income found for user !!" })
+
+    res.json({ allIncomes })
+  } catch (err) {
+    console.error(err.message)
+    return res.status(503).json({ message: err.message })
+  }
+})
+
+// update
+router.put("/edit/:id", async (req, res) => {
+  try {
+    const { id } = req.params
+    const { title, userId, date, amount } = req.body
+
+    if (req.userId !== userId) {
+      return res.status(400).json({ message: "Forbidden. Access Denied !!" })
+    }
+
+    const income = await prisma.income.update({
+      where: { id: parseInt(id) },
+      data: { title, date, amount },
+    })
+
+    if (!income)
+      return res
+        .status(404)
+        .json({ message: "No income found with this id !!" })
+
+    res.json({ message: "Income updated" })
+  } catch (err) {
+    console.error(err.message)
+    return res.status(503).json({ message: err.message })
+  }
+})
+
+// delete
+router.put("/delete/:id", async (req, res) => {
+  try {
+    const { id } = req.params
+
+    const income = await prisma.income.update({
+      where: { id: parseInt(id) },
+      data: { isDeleted: true },
+    })
+
+    if (!income) return res.status(404).json({ message: "No income found !!" })
+
+    res.json({ message: "Income Deleted !!" })
+  } catch (err) {
+    console.error(err.message)
+    return res.status(503).json({ message: err.message })
+  }
+})
+
+export default router
---
+++ backend/src/routes/tranactionRoutes.js
@@ -0,0 +1,135 @@
+import { Router } from "express"
+import { transactionValidation } from "../validator/validations.js"
+import { validationError } from "../validator/error.js"
+import prisma from "../prismaClient.js"
+
+const router = Router()
+
+// create
+router.post("/create", async (req, res) => {
+  try {
+    const { error, value } = transactionValidation.validate(req.body)
+
+    if (error) {
+      return validationError(res, error)
+    }
+
+    const { title, userId, date, amount } = value
+
+    if (req.userId !== userId) {
+      return res.status(400).json({ message: "Forbidden. Access Denied !!" })
+    }
+
+    const transaction = await prisma.transaction.create({
+      data: {
+        title,
+        userId,
+        date,
+        amount,
+      },
+    })
+
+    console.log(transaction)
+
+    if (!transaction || !transaction.id) {
+      return res
+        .status(400)
+        .json({ message: "Failed to create a transaction !!" })
+    }
+
+    res.json({ message: "Transaction added !!" })
+  } catch (err) {
+    console.error(err.message)
+    return res.status(503).json({ message: err.message })
+  }
+})
+
+// read
+router.get("/read/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params
+
+    if (req.userId !== parseInt(userId)) {
+      return res.status(400).json({ message: "Forbidden. Access Denied !!" })
+    }
+
+    const allTransactions = await prisma.transaction.findMany({
+      where: { userId: parseInt(userId) },
+    })
+
+    if (!allTransactions)
+      return res
+        .status(404)
+        .json({ message: "No transaction found for user !!" })
+
+    res.json({ allTransactions })
+  } catch (err) {
+    console.error(err.message)
+    return res.status(503).json({ message: err.message })
+  }
+})
+
+// update
+router.put("/edit/:id", async (req, res) => {
+  try {
+    const { id } = req.params
+    const { title, userId, date, amount } = req.body
+
+    if (req.userId !== userId) {
+      return res.status(400).json({ message: "Forbidden. Access Denied !!" })
+    }
+
+    const transaction = await prisma.transaction.update({
+      where: { id: parseInt(id) },
+      data: { title, date, amount },
+    })
+
+    if (!transaction)
+      return res
+        .status(404)
+        .json({ message: "No transaction found with this id !!" })
+
+    res.json({ message: "Transaction updated" })
+  } catch (err) {
+    console.error(err.message)
+    return res.status(503).json({ message: err.message })
+  }
+})
+
+// delete
+router.put("/delete/:id", async (req, res) => {
+  try {
+    const { id } = req.params
+
+    const transaction = await prisma.transaction.findUnique({
+      where: { id: parseInt(id) },
+    })
+
+    if (!transaction) {
+      return res.status(404).json({ message: "No transaction found !!" })
+    }
+
+    if (transaction && transaction.isDeleted === true) {
+      return res
+        .status(400)
+        .json({ message: "Transaction is already deleted !!" })
+    }
+
+    await prisma.transaction.update({
+      where: { id: parseInt(id) },
+      data: {
+        isDeleted: true,
+      },
+    })
+
+    // if (!transaction)
+    //   return res.status(404).json({ message: "No transaction found !!" })
+
+    res.json({ message: "Transaction Deleted !!" })
+  } catch (err) {
+    console.error(err.message)
+    return res.status(503).json({ message: err.message })
+  }
+})
+
+export default router
