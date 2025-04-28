157da4cb3bb5412016b6976063bb205fe9a0a990
Pulkit-s21 <pulkitsaxena795@gmail.com>
2025-04-28 16:39:28 +0530

    Income Validation created

--- backend/src/validator/validations.js
+++ backend/src/validator/validations.js
@@ -10,3 +10,17 @@ export const loginValidation = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().required(),
 }).options({ abortEarly: false })
+
+export const transactionValidation = Joi.object({
+  title: Joi.string().required(),
+  userId: Joi.number().required(),
+  date: Joi.date().iso().required(),
+  amount: Joi.number().required(),
+}).options({ abortEarly: false })
+
+export const incomeValidation = Joi.object({
+  title: Joi.string().required(),
+  userId: Joi.number().required(),
+  date: Joi.date().iso().required(),
+  amount: Joi.number().required(),
+}).options({ abortEarly: false })
