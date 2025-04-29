2aeaf6796d157e11534a96ec62b302288611a7ff
Pulkit-s21 <pulkitsaxena795@gmail.com>
2025-04-29 22:43:00 +0530

    Minor change for stopping errors in logs

--- frontend/src/components/SidebarLinks.jsx
+++ frontend/src/components/SidebarLinks.jsx
@@ -1,7 +1,7 @@
 export const SidebarLinks = ({ action, logo, title }) => {
   return (
     <li onClick={action} className="flex gap-4 cursor-pointer">
-      <img src={logo} alt="Icon" />
+      {/* <img src={logo} alt="Icon" /> */}
       <p>{title}</p>
     </li>
   )
--- frontend/src/components/UserInfo.jsx
+++ frontend/src/components/UserInfo.jsx
@@ -15,11 +15,11 @@ export const UserInfo = () => {
   return (
     <div className="flex flex-col items-center p-4 gap-8 bg-white w-[20%]">
       <div className="flex flex-col gap-2">
-        <img
+        {/* <img
           src={user?.image}
           alt="User profile"
           className="w-12 h-12 rounded-full border-2"
-        />
+        /> */}
         <p>{user?.username}</p>
       </div>
 
