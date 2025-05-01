e1e76019a01b8d72a04ec820c989c231fbcf6d93
Pulkit-s21 <pulkitsaxena795@gmail.com>
2025-05-01 22:13:31 +0530

    Task list updated. Expense path added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Login from "./pages/Login.jsx"
 import ProtectedRoutes from "./components/ProtectedRoutes.jsx"
 import { Home } from "./pages/Home.jsx"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Expense } from "./pages/Expense.jsx"
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<ProtectedRoutes element={<Home />} />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/expense" element={<Expense />} />
       </Routes>
     </Router>
   )
@@ -32,9 +34,11 @@ export default App
 // : Create services from transaction & income APIs : DONE
 // : Show actual data from QuickInfoBox from BE : DONE
 // TODO: Find a way to reload func when transaction OR income is added
-// TODO: Figure out how to show % for each column
+// : Figure out how to show % for each column : DONE
 // TODO: If token changes then kick out user for invalid token
 // : Add all other services for transaction : DONE
 // : Add all other services for income : DONE
 // : Pass limit to recent transactions to show only few at first : DONE
 // : Return transactions in descending order from BE : DONE
+// : Add diff pages for sidebar links : DONE
+// TODO: Showing sidebar on each page by importing on all. Try a diff method where it doesnt reloads and part on right does
