bc616cd517987defcfef7d5b632be27da8c5b0a4
Pulkit-s21 <pulkitsaxena795@gmail.com>
2025-05-01 22:12:52 +0530

    Expense Tab basic structure

---
+++ frontend/src/pages/Expense.jsx
@@ -0,0 +1,11 @@
+import { UserInfo } from "../components/UserInfo"
+
+export const Expense = () => {
+  return (
+    <div className="bg-gray-200 h-dvh text-black font-semibold flex gap-6">
+      <UserInfo />
+
+      <div>Hello</div>
+    </div>
+  )
+}
