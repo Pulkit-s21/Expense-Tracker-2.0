a9acd24d9fc0174161d8072201625d9ce59f284e
Pulkit-s21 <pulkitsaxena795@gmail.com>
2025-05-04 18:10:59 +0530

    Minor css change

--- frontend/src/components/OverviewArea.jsx
+++ frontend/src/components/OverviewArea.jsx
@@ -1,6 +1,6 @@
 export const OverviewArea = ({ component, heading }) => {
   return (
-    <div className="flex flex-col gap-4 bg-white w-[60%] p-4">
+    <div className="flex flex-col gap-4 bg-white w-[50%] p-4 shadow-xl rounded-xl">
       <p className="font-thin">{heading}</p>
 
       {component}
