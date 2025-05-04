522f03986b75b0aadea5820e4ecdc212366ccebf
Pulkit-s21 <pulkitsaxena795@gmail.com>
2025-05-04 18:10:46 +0530

    Icon added, css change for amount

--- frontend/src/components/RecentTransaction.jsx
+++ frontend/src/components/RecentTransaction.jsx
@@ -1,8 +1,9 @@
 import moment from "moment"
+import { FaArrowTrendDown, FaArrowTrendUp } from "react-icons/fa6"
 
-export const RecentTransaction = ({ logo, title, date, amount }) => {
+export const RecentTransaction = ({ logo, title, date, amount, type }) => {
   return (
-    <div className="flex justify-between">
+    <div className="flex justify-between items-center hover:bg-gray-100 p-2 rounded-md">
       <div className="flex items-center gap-4">
         <img src={logo} alt="Icon" />
         <div className="flex flex-col">
@@ -12,9 +13,23 @@ export const RecentTransaction = ({ logo, title, date, amount }) => {
           </p>
         </div>
       </div>
-      <div className={`bg-red-100 h-fit px-2 rounded-md`}>
-        <p className={`text-red-500`}>
-          <span>-$ {amount}</span>
+      <div
+        className={`${
+          type === "expense" ? "bg-red-100" : "bg-green-200"
+        }  h-fit px-2 rounded-sm`}
+      >
+        <p
+          className={`${
+            type === "expense" ? "text-red-500" : "text-green-500"
+          } flex items-center gap-2`}
+        >
+          <span className="font-light">
+            {type === "expense" ? "-" : "+"} $ {amount}
+          </span>
+
+          <span>
+            {type === "expense" ? <FaArrowTrendDown /> : <FaArrowTrendUp />}
+          </span>
         </p>
       </div>
     </div>
