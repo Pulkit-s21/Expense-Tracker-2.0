2739691a3bd3e84572683b0f56d7123bcd4585e2
Pulkit-s21 <pulkitsaxena795@gmail.com>
2025-05-04 20:41:26 +0530

    Added icons to sidebar links

--- frontend/src/components/SidebarLinks.jsx
+++ frontend/src/components/SidebarLinks.jsx
@@ -1,8 +1,11 @@
 export const SidebarLinks = ({ action, logo, title }) => {
   return (
-    <li onClick={action} className="flex gap-4 cursor-pointer">
-      {/* <img src={logo} alt="Icon" /> */}
-      <p>{title}</p>
+    <li
+      onClick={action}
+      className="flex gap-4 cursor-pointer items-center text-xl bg-violet-600 hover:bg-violet-300 transition-all text-white px-6 py-2 rounded-md"
+    >
+      <span className="text-2xl">{logo}</span>
+      <p className="font-medium">{title}</p>
     </li>
   )
 }
--- frontend/src/components/UserInfo.jsx
+++ frontend/src/components/UserInfo.jsx
@@ -2,20 +2,36 @@ import { useContext } from "react"
 import { UserContext } from "../helpers/UserContext"
 import { SidebarLinks } from "./SidebarLinks"
 import { useNavigate } from "react-router-dom"
+import { LuLayoutDashboard } from "react-icons/lu"
+import { IoWalletOutline } from "react-icons/io5"
+import { GiTakeMyMoney } from "react-icons/gi"
+import { IoLogOutOutline } from "react-icons/io5"
 
 export const UserInfo = () => {
   const { user, logout } = useContext(UserContext)
   const navigate = useNavigate()
 
   const sidebarLinks = [
-    { logo: "", title: "Dashboard", action: () => navigate("/") },
-    { logo: "", title: "Income", action: () => navigate("/income") },
-    { logo: "", title: "Expense", action: () => navigate("/expense") },
-    { logo: "", title: "Logout", action: logout },
+    {
+      logo: <LuLayoutDashboard />,
+      title: "Dashboard",
+      action: () => navigate("/"),
+    },
+    {
+      logo: <IoWalletOutline />,
+      title: "Income",
+      action: () => navigate("/income"),
+    },
+    {
+      logo: <GiTakeMyMoney />,
+      title: "Expense",
+      action: () => navigate("/expense"),
+    },
+    { logo: <IoLogOutOutline />, title: "Logout", action: logout },
   ]
 
   return (
-    <div className="flex flex-col items-center p-4 gap-8 bg-white w-[20%]">
+    <div className="flex flex-col items-center p-4 gap-8 bg-white w-[15%]">
       <div className="flex flex-col gap-2">
         {/* <img
           src={user?.image}
@@ -26,7 +42,7 @@ export const UserInfo = () => {
       </div>
 
       {/* links */}
-      <ul className="flex flex-col gap-2">
+      <ul className="flex flex-col gap-3">
         {sidebarLinks.map((link, idx) => {
           return (
             <SidebarLinks
