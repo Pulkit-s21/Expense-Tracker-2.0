d3d8c292a975b2e103da464cde64a7ce42e0ddef
Pulkit-s21 <pulkitsaxena795@gmail.com>
2025-05-04 18:11:28 +0530

    Passing info for child components

--- frontend/src/components/DetailArea.jsx
+++ frontend/src/components/DetailArea.jsx
@@ -1,17 +1,71 @@
 import { OverviewArea } from "./OverviewArea"
 import { QuickInfoArea } from "./QuickInfoArea"
 import { RecentTransactionArea } from "./RecentTransactionArea"
+import { getUserTransactions } from "../services/authServices"
+import { getTransactions } from "../services/transactionServices"
+import { getIncomes } from "../services/incomeServices"
+import { Chart } from "./Chart"
+import Barchart from "./Barchart"
 
 export const DetailArea = () => {
+  const limit = 7
   return (
-    <div className="flex flex-col w-[80%] pr-4">
+    <div className="flex flex-col w-[80%] pr-4 py-6">
       <QuickInfoArea />
 
       {/* recent & chart */}
       <div className="flex gap-4 mt-10">
-        <RecentTransactionArea />
+        <RecentTransactionArea
+          detailFunc={getUserTransactions}
+          heading={"Recent Transactions"}
+          limit={limit}
+        />
 
-        <OverviewArea />
+        <OverviewArea heading={"Financial Overview"} component={<Chart />} />
+      </div>
+
+      {/* expense & chart */}
+      <div className="flex gap-4 mt-10">
+        <RecentTransactionArea
+          detailFunc={getTransactions}
+          heading={"Recent Expenses"}
+          limit={limit}
+          page={"/expense"}
+        />
+
+        <OverviewArea
+          heading={"Last 30 days expenses"}
+          component={
+            <Barchart
+              type="expense"
+              label="Expense"
+              id="expenseId"
+              color="red"
+            />
+          }
+        />
+      </div>
+
+      {/* income & chart */}
+      <div className="flex gap-4 mt-10">
+        <OverviewArea
+          heading={"Last 30 days incomes"}
+          component={
+            <Barchart
+              type="income"
+              label="Income"
+              id="incomeId"
+              color="lightGreen"
+            />
+          }
+        />
+
+        <RecentTransactionArea
+          detailFunc={getIncomes}
+          heading={"Recent Incomes"}
+          limit={limit}
+          page={"/income"}
+        />
       </div>
     </div>
   )
