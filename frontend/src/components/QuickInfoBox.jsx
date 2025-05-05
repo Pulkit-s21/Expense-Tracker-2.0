899e6d4f5aedb1c2294899e7f4faca30c09fe910
Pulkit-s21 <pulkitsaxena795@gmail.com>
2025-05-05 16:57:46 +0530

    Icon added, minor css change

--- frontend/src/components/QuickInfoBox.jsx
+++ frontend/src/components/QuickInfoBox.jsx
@@ -1,20 +1,12 @@
-export const QuickInfoBox = ({ logo, title, amount, type }) => {
+export const QuickInfoBox = ({ logo, title, amount, color }) => {
   return (
-    <div className="flex-1 flex items-center bg-white px-6 py-4 rounded-lg gap-6 shadow-xl">
-      {/* <img src={logo} alt="Icon" /> */}
+    <div className="flex-1 flex items-center bg-white px-6 py-4 rounded-lg gap-4 shadow-xl">
+      <span className={`text-4xl rounded-full ${color} p-4`}>{logo}</span>
       <div className="flex-col items-center text-center">
-        <p className="text-sm font-medium text-gray-400">{title}</p>
-        <p className="lg:text-2xl text-gray-300">
+        <p className="text-sm font-medium text-gray-500">{title}</p>
+        <p className="lg:text-2xl text-gray-900">
           ${" "}
-          <span
-            className={`lg:text-2xl font-bold ${
-              type === "transaction"
-                ? "text-red-500"
-                : type === "income"
-                ? "text-green-500"
-                : "text-blue-500"
-            }`}
-          >
+          <span className={`lg:text-2xl font-medium text-gray-900`}>
             {amount}
           </span>
         </p>
