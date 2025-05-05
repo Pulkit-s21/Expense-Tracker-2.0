1a6699b9f86057e9ab4502c0fed91cbb824bd10e
Pulkit-s21 <pulkitsaxena795@gmail.com>
2025-05-05 17:02:14 +0530

    Filter prop to choose whether to show 30 days or all. Custom xAxis and tooltip content

--- frontend/src/components/Barchart.jsx
+++ frontend/src/components/Barchart.jsx
@@ -2,7 +2,7 @@ import { BarChart } from "@mui/x-charts/BarChart"
 import { useContext } from "react"
 import { UserContext } from "../helpers/UserContext"
 
-export const Barchart = ({ type, label, id, color }) => {
+export const Barchart = ({ type, label, id, color, filter = true }) => {
   const { userNumbers } = useContext(UserContext)
 
   // bcz we only show last month on dashboard
@@ -11,19 +11,47 @@ export const Barchart = ({ type, label, id, color }) => {
 
   const data =
     userNumbers[type]
-      ?.filter((item) => new Date(item.date) >= thirtyDaysAgo)
+      ?.filter((item) => (filter ? new Date(item.date) >= thirtyDaysAgo : true))
       .map((item) => item.amount) || []
 
   const xLabels =
     userNumbers[type]
-      ?.filter((item) => new Date(item.date) >= thirtyDaysAgo)
+      ?.filter((item) => (filter ? new Date(item.date) >= thirtyDaysAgo : true))
+      .map((item) =>
+        item.title.length > 3 ? item.title.slice(0, 3) + "..." : item.title
+      ) || []
+
+  const fullLabels =
+    userNumbers[type]
+      ?.filter((item) => (filter ? new Date(item.date) >= thirtyDaysAgo : true))
       .map((item) => item.title) || []
+
+  const fullLabelFormatter = (index) => fullLabels[index]
 
   return (
     <BarChart
       height={300}
-      series={[{ data: data, label: label, id: id, color: color }]}
-      xAxis={[{ scaleType: "band", data: xLabels }]}
+      series={[
+        {
+          data: data,
+          label: label,
+          id: id,
+          color: color,
+          valueFormatter: (value, context) => {
+            const index = context.dataIndex
+            return `${fullLabelFormatter(index)}: $ ${value}`
+          },
+        },
+      ]}
+      xAxis={[
+        {
+          scaleType: "band",
+          data: xLabels,
+          label: "Activities",
+          valueFormatter: (value, index) => fullLabelFormatter(index),
+        },
+      ]}
+      yAxis={[{ label: "Amount" }]}
     />
   )
 }
