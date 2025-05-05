4be156a0e179bd216e5a2c60ba9f8fedc202e129
Pulkit-s21 <pulkitsaxena795@gmail.com>
2025-05-05 16:56:57 +0530

    Income & expense page

--- frontend/src/pages/Income.jsx
+++ frontend/src/pages/Income.jsx
@@ -1,17 +1,17 @@
-import Barchart from "../components/Barchart"
 import { UserInfo } from "../components/UserInfo"
+import { Barchart } from "../components/Barchart"
 
-export const Expense = () => {
+export const Income = () => {
   return (
     <div className="bg-gray-50 h-full text-black font-semibold flex gap-6">
       <UserInfo />
 
       <div className="flex flex-col gap-4 py-6 pr-4 w-full">
         <Barchart
-          type="expense"
-          label="Expense"
-          id="expenseId"
-          color="red"
+          type="income"
+          label="Income"
+          id="incomeId"
+          color="lightGreen"
           filter={false}
         />
       </div>
